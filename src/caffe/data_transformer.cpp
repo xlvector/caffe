831811a839b589d27c84d21c4ddf408dcac20402
xlvector <xlvector@gmail.com>
2016-04-28 10:28:51 +0800

    need rng

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -527,7 +527,8 @@ vector<int> DataTransformer<Dtype>::InferBlobShape(
 template <typename Dtype>
 void DataTransformer<Dtype>::InitRand() {
   const bool needs_rand = param_.mirror() ||
-      (phase_ == TRAIN && param_.crop_size());
+      (phase_ == TRAIN && param_.crop_size()) ||
+      param_.color();
   if (needs_rand) {
     const unsigned int rng_seed = caffe_rng_rand();
     rng_.reset(new Caffe::RNG(rng_seed));
